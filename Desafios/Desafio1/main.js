e74a73bf9ed927fd8f083b33de0ff788f238b1de
Milena Zoe Aberbach <milena.aberbach@gmail.com>
2024-04-19 20:24:52 -0300

    Terminé el desafio 1

--- Desafios/Desafio1/main.js
+++ Desafios/Desafio1/main.js
@@ -1,4 +1,10 @@
-let nombreProducto = 0;
-let precioUnitario = 0;
-let cantidadDeseada = 0;
+let nombreProducto = "Coca Cola";
+let precioUnitario = 1200;
+let cantidadDeseada = prompt("Cantidad deseada de Coca Cola: "); //Pregunta al usuario por consola cuantas unidades quiere
+
+let costoTotal = precioUnitario * cantidadDeseada;
+
+alert("El costo total de la compra del usuario es: " + costoTotal);
+
+
 

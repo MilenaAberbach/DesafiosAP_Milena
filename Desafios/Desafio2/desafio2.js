bb4ce768b717f8bd17b24c8b315ff5eb724db37e
Milena Zoe Aberbach <milena.aberbach@gmail.com>
2024-04-26 20:14:57 -0300

    Termine el Desafio 2

---
+++ Desafios/Desafio2/desafio2.js
@@ -0,0 +1,14 @@
+let nombreProducto = "Chocolate";
+let precioUnitario = 300;
+let cantidadDeseada = prompt("Cantidad deseada de Chocolate: ");
+const costoTotal = precioUnitario * cantidadDeseada;
+
+alert(`Usted compro ${cantidadDeseada} unidades de ${nombreProducto}. El costo total es: $${costoTotal}`);
+
+if(cantidadDeseada >= 5){
+
+    alert(`Tenes un 10% de descuento!!!`)
+
+    precio_conDescuento = costoTotal - ((costoTotal * 10)/100);
+    Swal.fire(`El costo total, ahora aplicado un 10% de descuento, es: $${precio_conDescuento}`);
+}

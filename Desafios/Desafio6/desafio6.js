6e12f759566958b7f927282317ef6fb424f8ad9c
Milena Zoe Aberbach <milena.aberbach@gmail.com>
2024-05-24 19:21:28 -0300

    Termine el desafio 6

---
+++ Desafios/Desafio6/desafio6.js
@@ -0,0 +1,7 @@
+const nuevoPrecio_prod = document.getElementById("precioProducto");
+
+nuevoPrecio_prod.innerHTML = "Precio: $450";
+nuevoPrecio_prod.style.fontFamily="Montserrat, sans-serif"
+nuevoPrecio_prod.style.fontWeight="500"
+nuevoPrecio_prod.style.fontSize="25px"
+console.log(nuevoPrecio_prod);
